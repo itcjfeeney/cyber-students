281573601e73f7ab29856f7d4a05275868de630c
ictjfeeney <C00279785@itcarlow.ie>
2022-05-08 08:44:21 +0100

    Added code to dycrypt user details

--- api/handlers/user.py
+++ api/handlers/user.py
@@ -1,16 +1,35 @@
+#Added code to dycrypt user details
+
+import os
+
 from tornado.escape import json_decode, utf8
 from tornado.gen import coroutine
 from tornado.web import authenticated
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
 from .auth import AuthHandler
+
+def aes_decrypt(ciphertext):
+    key = "thebestsecretkey"
+    key_bytes = bytes(key, "utf-8")
+    aes_cipher = Cipher(algorithms.AES(key_bytes),
+                        modes.ECB(),
+                        backend=default_backend())
+    aes_decryptor = aes_cipher.decryptor()
+
+    plaintext_bytes = aes_decryptor.update(ciphertext_bytes)
+    plaintext = str(plaintext_bytes, "utf-8")
+    return plaintext
 
 class UserHandler(AuthHandler):
 
     @authenticated
     def get(self):
         self.set_status(200)
-        self.response['email'] = self.current_user['email']
-        self.response['displayName'] = self.current_user['display_name']
+        self.response['email'] = aes_decrypt(bytes.fromhex(self.current_user['email']))
+        self.response['displayName'] = aes_decrypt(bytes.fromhex(self.current_user['display_name']))
         self.write_json()
 
     @coroutine
